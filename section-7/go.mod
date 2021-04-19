7b214bcb6c5f34cb0564beb236413e7e7e326ff3
Blaine Richardson <blainerrichardson@gmail.com>
2021-04-18 19:49:07 -0700

    feat: section-8: finishing section 8 and all of course

---
+++ section-7/go.mod
@@ -0,0 +1,3 @@
+module go-the-complete-developers-guide-section-7
+
+go 1.16
---
+++ section-7/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	links := []string{
+		"https://www.google.com",
+		"https://www.facebook.com",
+		"https://www.stackoverflow.com",
+		"https://www.golang.org",
+		"https://www.amazon.com",
+	}
+
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	for l := range c {
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			checkLink(link, c)
+		}(l)
+	}
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		m := link + " might be down - received error " + err.Error()
+		fmt.Println(m)
+		c <- link
+		return
+	}
+	m := link + " is up"
+	fmt.Println(m)
+	c <- link
+}
